1863c04c0d6be7af1ae4453fb9955c981fc71581
AnuragOP01 <ranurag404@gmail.com>
2025-11-26 17:09:33 +0530

    fixed build error

--- src/app/astrologer/edit-report-astrologer/page.tsx
+++ src/app/astrologer/edit-report-astrologer/page.tsx
@@ -1,12 +1,20 @@
-import ReportAstrologerForm from '@/components/AstrologerReportManagement'
-import React from 'react'
+// app/astrologer/edit-report-astrologer/page.tsx
+import ReportAstrologerForm from '@/components/AstrologerReportManagement';
+import { Suspense } from 'react';
+import React from 'react';
 
-const page = () => {
+const Page = () => {
   return (
     <div>
-        <ReportAstrologerForm/>
+      <Suspense fallback={
+        <div className="flex items-center justify-center min-h-screen">
+          <div className="animate-spin rounded-full h-12 w-12 border-4 border-gray-200 border-t-red-500"></div>
+        </div>
+      }>
+        <ReportAstrologerForm />
+      </Suspense>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default Page;
